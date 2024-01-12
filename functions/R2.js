3ada56a6f9cc35188eda83aae453a57442c6c69c
Neeranuch Jitkhajornwanich <jitkhajornwanich.n@gmail.com>
2024-01-12 22:48:16 +0700

    find request

--- functions/R2.js
+++ functions/R2.js
@@ -88,7 +88,7 @@ export async function onRequestGet(request) {
             headers: { 'Content-Type': 'image/png' },
         });
     } catch (error) {
-        return new Response(`Error: ${error.message} + ${request.url}`, { status: 500 });
+        return new Response(`Error: ${error.message} + ${request}`, { status: 500 });
     }
 }
 
